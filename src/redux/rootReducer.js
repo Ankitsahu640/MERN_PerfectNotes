09bf60a0283676cec1631bd18924f880f98e1db9
Ankitsahu640 <107627388+Ankitsahu640@users.noreply.github.com>
2023-02-26 19:29:37 +0530

    Update rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import loadReducer from './Loading/reduce'
 import reducer from './Notes/reduce'
 import shareReducer from './ShareNotes/reduce'
 import userReducer from './Users/reducer'
@@ -6,7 +7,8 @@ import userReducer from './Users/reducer'
 const rootReducer = combineReducers({
   notes: reducer,
   user: userReducer,
-  share: shareReducer
+  share: shareReducer,
+  load : loadReducer
 })
 
 export default rootReducer
